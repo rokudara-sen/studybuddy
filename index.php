76fd30a12ff1734dd9f70da654dd6d06f4061ee9
if23b266 <if23b266@technikum-wien.at>
2024-03-21 16:25:01 +0100

    Footer und FAQ Final

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Study Buddy</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="res/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -11,28 +14,37 @@
 </head>
 
 <body>
-    <?php include 'inc/header.php'; ?>
+    <?php require_once 'config/session.php';
+    include 'inc/header.php'; ?>
     <div id="content">
         <?php
 
-        if (isset ($_GET['action']) && !empty ($_GET['action'])) {
-            switch ($_GET['action']) {
+        if (isset ($_GET['page']) && !empty ($_GET['page'])) {
+            switch ($_GET['page']) {
                 case 'home':
                     include 'inc/home.php';
                     break;
+                case 'profile':
+                    include 'inc/profile.php';
+                    break;
+                case 'registration':
+                    include 'inc/registration.php';
+                    break;
+                case 'login':
+                    include 'inc/login.php';
+                    break;
+                case 'imprint':
+                    include 'inc/imprint.php';
+                    break;
                 case 'faq':
                     include 'inc/faq.php';
-                    break;
+                // default:
+        
             }
         }
-
         ?>
     </div>
     <?php include 'inc/footer.php'; ?>
-    <script src="res/js/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
