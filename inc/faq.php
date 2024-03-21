72cc7dffdf490fb82b803a5ad14c83a7305d3f2e
if23b266 <if23b266@technikum-wien.at>
2024-03-21 16:09:40 +0100

    Footer.php und FAQ.php fertig

--- inc/faq.php
+++ inc/faq.php
@@ -10,50 +10,23 @@
 </head>
 
 <body>
-    <p>
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample1"
-            aria-expanded="false" aria-controls="collapseWidthExample1">
-            How do I create an account?
-        </button>
-    </p>
-    <div style="min-height: 120px;">
-        <div class="collapse collapse-horizontal" id="collapseWidthExample1">
-            <div class="card card-body" style="width: 300px;">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione maxime asperiores magnam distinctio
-                eius repellat sequi, itaque est! Nesciunt natus libero culpa placeat eius voluptate quis mollitia est
-                porro sapiente.
-            </div>
-        </div>
-    </div>
-    <p>
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample2"
-            aria-expanded="false" aria-controls="collapseWidthExample2">
-            How do I create an account?
-        </button>
-    </p>
-    <div style="min-height: 120px;">
-        <div class="collapse collapse-horizontal" id="collapseWidthExample2">
-            <div class="card card-body" style="width: 300px;">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione maxime asperiores magnam distinctio
-                eius repellat sequi, itaque est! Nesciunt natus libero culpa placeat eius voluptate quis mollitia est
-                porro sapiente.
-            </div>
-        </div>
-    </div>
-    <p>
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample3"
-            aria-expanded="false" aria-controls="collapseWidthExample3">
-            How do I create an account?
-        </button>
-    </p>
-    <div style="min-height: 120px;">
-        <div class="collapse collapse-horizontal" id="collapseWidthExample3">
-            <div class="card card-body" style="width: 300px;">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione maxime asperiores magnam distinctio
-                eius repellat sequi, itaque est! Nesciunt natus libero culpa placeat eius voluptate quis mollitia est
-                porro sapiente.
-            </div>
-        </div>
+    <div id="helpDiv" class="d-flex flex-column">
+
+        <h1>Frequently asked Questions</h1>
+
+
+        <details class="rounded">
+            <summary class="rounded ">How do I create an account?</summary>
+            <p>Click on the "Sign Up" button at the top right corner of the homepage. Fill in your details and follow the instructions. A confirmation email will be sent to you. Confirm your email to complete the registration process.</p>
+        </details>
+        <details class="rounded">
+            <summary class="rounded ">Can I change my password?</summary>
+            <p>Yes, you can change your password. Once logged in, go to your account settings and select "Change Password". Enter your current password and then your new password twice to confirm. Click "Save" to update your password.</p>
+        </details>
+        <details class="rounded">
+            <summary class="rounded ">How can I contact customer support?</summary>
+            <p>You can reach our customer support team by emailing support@website.com or by using the live chat feature on our website. Our team is available 24/7 to assist you with any queries or issues.</p>
+        </details>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 
@@ -8,28 +7,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="res/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
+
 <body>
+    <?php include 'inc/header.php'; ?>
+    <div id="content">
+        <?php
 
-<?php
-include 'inc/header.php';
+        if (isset ($_GET['action']) && !empty ($_GET['action'])) {
+            switch ($_GET['action']) {
+                case 'home':
+                    include 'inc/home.php';
+                    break;
+                case 'faq':
+                    include 'inc/faq.php';
+                    break;
+            }
+        }
 
-if (isset($_GET['action']) && !empty($_GET['action'])) {
-    switch ($_GET['action']) {
-        case 'home':
-            include 'inc/home.php';
-            break;
-        case 'faq':
-            include 'inc/faq.php';
-            break;
-    }
-}
-
-include 'inc/footer.php';
-?>
-
-<script src="res/js/script.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        ?>
+    </div>
+    <?php include 'inc/footer.php'; ?>
+    <script src="res/js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
