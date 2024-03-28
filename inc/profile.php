24391706502c47d0599da2a217c788e8f922cf1b
Theo Wendel <if23b239@technikum-wien.at>
2024-03-28 13:00:20 +0100

    Aenderungen Profil

--- inc/profile.php
+++ inc/profile.php
@@ -1,25 +1,50 @@
 <?php
-   //check which/if session is set
-
+    // if ($_SESSION['userrole'] == "anonym") {
+    //  header("Location: index.php?page=home");
+     // exit();
+    //}
 ?>
 
-<div>
-    <form action="">
+<!doctype html>
+<html lang="en">
 
-        <input type="text" name="profileMajor" id="profileMajor" placeholder="MAJOR ändern"><br>
-
-        <input type="text" name="profileAge" id="profileAge" placeholder="AGE ändern"><br>
-
-        <textarea name="profileText" rows="4" cols="50" placeholder="Text ändern"></textarea> <br>
-
-        <input type="file" id="profilePic" name="profilePic"><br><br>
-
-        <button type="submit">Ändern</button>
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+    <h2 class="p-3 mt-3" >Profil ändern</h2>
+    <form class="p-3 mt-3" action="config/changeProfile.php" method="POST">
+        <div class="mb-3">
+            <input type="text" class="form-control" name="profileName" id="profileName" placeholder="Name ändern">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="profileMajor" id="profileMajor" placeholder="MAJOR ändern">
+        </div>
+        <div class="mb-3">
+            <input type="text" class="form-control" name="profileAge" id="profileAge" placeholder="AGE ändern">
+        </div>
+        <div class="mb-3">
+            <textarea class="form-control" name="profileText" id="profileText" rows="4" cols="50" placeholder="Text ändern"></textarea>
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="file" id="profilePic" name="profilePic">
+        </div>
+        <br>
+        <button class="form-control" id="profileButton" type="submit">Ändern</button>
     </form>
-
-
-
-
-
+    </div>
+  </div>
 </div>
+<br><br><br>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous">
+</script>
+</body>
+</html>
