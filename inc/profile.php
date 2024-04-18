bc400d0b1f227fc0a286f705933a28346186d73f
Theo Wendel <if23b239@technikum-wien.at>
2024-04-18 18:19:45 +0200

    Fehler verbessert + datenbankchange commitet

--- inc/profile.php
+++ inc/profile.php
@@ -1,7 +1,7 @@
 <?php
     if ($_SESSION['userrole'] == "anonym") {
-    //header("Location: index.php?page=home");
-     //exit();
+    header("Location: index.php?page=home");
+     exit();
     }
 ?>
 
@@ -19,7 +19,7 @@
   <div class="row">
     <div class="col-md-6">
     <h2 class="p-3 mt-3" >Profil ändern</h2>
-    <form class="p-3 mt-3" action="config/changeProfile.php" method="POST">
+    <form class="p-3 mt-3" action="config/changeProfile.php" method="post">
         <div class="mb-3">
             <input type="text" class="form-control" name="profileName" id="profileName" placeholder="Profilname/Username ändern">
         </div>
@@ -41,7 +41,7 @@
     </div>
     <div class="col-md-6">
     <h2 class="p-3 mt-3" >Accountdaten ändern</h2>
-    <form class="p-3 mt-3" action="config/changeProfile.php" method="POST">
+    <form class="p-3 mt-3" action="config/changeProfile.php" method="post">
         <div class="mb-3">
             <input type="text" class="form-control" name="profileVorname" id="profileVorname" placeholder="Vorname ändern">
         </div>
@@ -51,7 +51,8 @@
         <div class="mb-3">
             <input type="email" class="form-control" name="profileEmail" id="profileEmail" placeholder="Email ändern">
         </div>
-
+        <br>
+        <button class="form-control" id="profileButton" type="submit">Ändern</button>
     </form>
     </div>
   </div>
