80b0b14d2bb1e795b7fc8a4f8dc28abe270cbc2b
rokudara-sen <160833839+rokudara-sen@users.noreply.github.com>
2024-06-23 17:30:20 +0200

    Update faq.php

--- inc/faq.php
+++ inc/faq.php
@@ -3,14 +3,26 @@
 
     <details class="rounded">
         <summary class="rounded">How do I create an account?</summary>
-        <p>Click on the "Sign Up" button at the top right corner of the homepage. Fill in your details and follow the instructions. A confirmation email will be sent to you. Confirm your email to complete the registration process.</p>
+        <p>Click on the "Registration" button at the top right corner of the homepage. Fill in your details and follow the instructions. A confirmation email will be sent to you. Confirm your email to complete the registration process.</p>
     </details>
     <details class="rounded">
         <summary class="rounded">Can I change my password?</summary>
-        <p>Yes, you can change your password. Once logged in, go to your account settings and select "Change Password". Enter your current password and then your new password twice to confirm. Click "Save" to update your password.</p>
+        <p>Yes, you can change your password. Once logged in, go to your profile settings and select "Change Password". Enter your current password and then your new password twice to confirm. Click "Save" to update your password.</p>
     </details>
     <details class="rounded">
         <summary class="rounded">How can I contact customer support?</summary>
-        <p>You can reach our customer support team by emailing support@website.com or by using the live chat feature on our website. Our team is available 24/7 to assist you with any queries or issues.</p>
+        <p>You can reach our customer support team by emailing admin@studybuddy-technikum.at. Our team is available 24/7 to assist you with any queries or issues.</p>
+    </details>
+    <details class="rounded">
+        <summary class="rounded">How does the matching work?</summary>
+        <p>In the Home Section, choose a Profile and click on the "send like" Button. If the other user likes you too, you can message each other.</p>
+    </details>
+    <details class="rounded">
+        <summary class="rounded">How does the reporting work?</summary>
+        <p>If you click the "report user" Button and type in the message, the admins will have a look at it.</p>
+    </details>
+    <details class="rounded">
+        <summary class="rounded">Where can I change my data?</summary>
+        <p>In the profile section you can add text, a picture and change all your data. A profile review is on the section too.</p>
     </details>
 </div>
